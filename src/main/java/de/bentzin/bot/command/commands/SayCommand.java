a5aa6934e66fa5fb734432ef58d526631ac8d0e5
TDR <tdr.minecraft@gmail.com>
2022-01-04 14:34:10 +0100

    Update SayCommand.java

--- src/main/java/de/bentzin/bot/command/commands/SayCommand.java
+++ src/main/java/de/bentzin/bot/command/commands/SayCommand.java
@@ -1,11 +1,11 @@
 package de.bentzin.bot.command.commands;
 
-import de.bentzin.facharbeit.bot.Bot;
-import de.bentzin.facharbeit.bot.command.Command;
-import de.bentzin.facharbeit.bot.command.CommandSystem;
-import de.bentzin.facharbeit.bot.command.Target;
-import de.bentzin.facharbeit.bot.message.MessageGenerator;
-import de.bentzin.facharbeit.bot.permission.Role;
+import de.bentzin.bot.UntisBot;
+import de.bentzin.bot.command.Command;
+import de.bentzin.bot.command.CommandSystem;
+import de.bentzin.bot.command.Target;
+import de.bentzin.bot.message.MessageGenerator;
+import de.bentzin.bot.permission.Role;
 import it.auties.whatsapp4j.protobuf.chat.Chat;
 import it.auties.whatsapp4j.protobuf.info.MessageInfo;
 import org.jetbrains.annotations.Nullable;
@@ -26,7 +26,7 @@ public class SayCommand extends Command {
 
     @Override
     public void execute(@Nullable Chat chat, String[] args, @Nullable MessageInfo label, boolean fromSystem, Optional sender) {
-        Bot.getApi().sendMessage(chat, MessageGenerator.generateBotTextMessage("Version: 0.00", label.container().textMessage().text().replace(CommandSystem.INITIATOR + getKey() + " ", "")));
+        UntisBot.getApi().sendMessage(chat, MessageGenerator.generateBotTextMessage("Version: 0.00", label.container().textMessage().text().replace(CommandSystem.INITIATOR + getKey() + " ", "")));
         return;
     }
 }
