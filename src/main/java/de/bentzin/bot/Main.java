b4ac68af41c488affee3eba008f183d75355ff71
TDR <tdr.minecraft@gmail.com>
2022-01-04 14:24:05 +0100

    start porting of de.bentzin.facharbeit

---
+++ src/main/java/de/bentzin/bot/Main.java
@@ -0,0 +1,12 @@
+package de.bentzin.bot;
+
+public class Main {
+
+    /**
+     *
+     * @param args 0: debugmode | boolean
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/de/bentzin/bot/UntisBot.java
@@ -0,0 +1,16 @@
+package de.bentzin.bot;
+
+public final class UntisBot {
+
+    private static void init(boolean debugmode) {
+
+    }
+
+    private static void connect() {
+
+    }
+
+    private static void terminate() {
+
+    }
+}
